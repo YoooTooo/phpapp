d44a79f72f6a583f23094f4260d6ff1a9878a973
yoootooo <son.toyo.mcmxc@gmail.com>
2020-11-18 22:28:42 +0900

    added somefiles

--- backend/resources/views/posts/index.blade.php
+++ backend/resources/views/posts/index.blade.php
@@ -22,6 +22,12 @@
                             コメント {{ $post->comments->count() }}件
                         </span>
                     @endif
+
+                    <div class="mb-4">
+                        <a href="{{ route('posts.create') }}" class="btn btn-primary">
+                            投稿を新規作成する
+                        </a>
+                    </div>
                 </div>
             </div>
         @endforeach
